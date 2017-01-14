802c61a42f9f32b7e6f61539be6bc233e8e9b0f4
Lukasz Biedak <l.biedak@wp.pl>
2017-01-14 11:49:04 +0100

    Improvements

--- src/test/TestReading.java
+++ src/test/TestReading.java
@@ -12,20 +12,23 @@ public class TestReading {
 
 	@Test
 	public void test() {
-		
+
 		List<String> input = new ArrayList<String>();
-		input.add("11 7 2");
+
+		input.add("11 10 5");
 		input.add("2 3 4 5 2 2");
 		input.add("2 6 3 1 1 3");
-		
-		
+		input.add("2 7 2 2 2 4");
+		input.add("5 5 10 3 1 5");
+		input.add("4 3 5 5 2 6");
+
 		InputReader ir = new InputReader();
 		Problem p = ir.ReadProblemFromInput(input);
 		p.analyze();
 		System.out.println(p);
-		
-		//add asserts
-		
+
+		// add asserts
+
 	}
 
 }
